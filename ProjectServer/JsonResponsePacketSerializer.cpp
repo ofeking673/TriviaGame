606c8867d168d19361fea874c48183d14d0a2301
unknown <neslioz07@gmail.com>
2024-04-04 15:35:30 +0300

    Added Serializer of Json Response Packet. within it added structs to implement error,login,signup responses

---
+++ ProjectServer/JsonResponsePacketSerializer.cpp
@@ -0,0 +1,79 @@
+#include "JsonResponsePacketSerializer.h"
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+/// <summary>
+/// Serialize Error response from JSON format to binary
+/// </summary>
+/// <param name="errorResponse">Error response object</param>
+/// <returns>Buffer contains binary format of JSON error response</returns>
+Buffer JsonResponsePacketSerializer::serializeResponse(const ErrorResponse& errorResponse)
+{
+    Buffer buffer;
+
+    // Convert ErrorResponse to JSON
+    json j;
+    j["message"] = errorResponse.message;
+
+    // Serialize the JSON object to a string
+    std::string serializedString = j.dump();
+
+    // Convert the string to Buffer
+    for (auto& ch : serializedString)
+    {
+        buffer.data.push_back(static_cast<unsigned char>(ch));
+    }
+
+    return buffer;
+}
+
+/// <summary>
+/// Serialize Login response from JSON format to binary
+/// </summary>
+/// <param name="loginResponse">Login response object</param>
+/// <returns>Buffer contains binary format of JSON login response</returns>
+Buffer JsonResponsePacketSerializer::serializeResponse(const LoginResponse& loginResponse)
+{
+    Buffer buffer;
+
+    // Convert LoginResponse to JSON
+    json j;
+    j["status"] = loginResponse.status;
+
+    // Serialize the JSON object to a string
+    std::string serializedString = j.dump();
+
+    // Convert the string to Buffer
+    for (auto& ch : serializedString)
+    {
+        buffer.data.push_back(static_cast<unsigned char>(ch));
+    }
+
+    return buffer;
+}
+
+/// <summary>
+/// Serialize Signup response from JSON format to binary
+/// </summary>
+/// <param name="signupResponse">Signup response object</param>
+/// <returns>Buffer contains binary format of JSON signup response</returns>
+Buffer JsonResponsePacketSerializer::serializeResponse(const SignupResponse& signupResponse)
+{
+    Buffer buffer;
+
+    // Convert SignupResponse to JSON
+    json j;
+    j["status"] = signupResponse.status;
+
+    // Serialize the JSON object to a string
+    std::string serializedString = j.dump();
+
+    // Convert the string to Buffer
+    for (auto& ch : serializedString)
+    {
+        buffer.data.push_back(static_cast<unsigned char>(ch));
+    }
+
+    return buffer;
+}
---
+++ ProjectServer/JsonResponsePacketSerializer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include "Buffer.h"
+
+
+// Struct of error response
+struct ErrorResponse
+{
+	// error messasge
+	std::string message;
+};
+
+// Struct of login response
+struct LoginResponse
+{
+	// login status
+	unsigned int status;
+};
+
+// Struct of signup response
+struct SignupResponse
+{
+	// signup status
+	unsigned int status;
+};
+
+
+// Static class to serialize json response packets
+class JsonResponsePacketSerializer
+{
+public:
+	static Buffer serializeResponse(const ErrorResponse& errorResponse);
+	static Buffer serializeResponse(const LoginResponse& loginResponse);
+	static Buffer serializeResponse(const SignupResponse& signupResponse);
+};
