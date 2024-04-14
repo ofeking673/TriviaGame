ac3141b24bf240eb4cf28f3c949c65b5eaeb51ec
unknown <neslioz07@gmail.com>
2024-04-14 12:29:23 +0300

    Added LoginManager header file

---
+++ ProjectServer/LoginManager.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "IDatabase.h"
+#include "LoggedUser.h"
+#include <vector>
+
+class LoginManager
+{
+public:
+	void signup(std::string username, std::string password, std::string email);
+	void login(std::string username, std::string password);
+	void logout(std::string username);
+
+private:
+	IDatabase* m_database;
+	std::vector<LoggedUser> m_loggedUsers;
+};
