7f70222a9e15e2d72bfc689dd6542ced9f81d853
ofeking475 <ofeking475@gmail.com>
2024-04-15 08:15:01 +0300

    BaseGuest now building requests and sending to server

--- baseGuest.py
+++ baseGuest.py
@@ -1,15 +1,32 @@
 import socket as s
+import json
+import sys
 
 def main():
     address = "127.0.0.1"
     port = 8826
     with s.socket(s.AF_INET, s.SOCK_STREAM) as sock:
         sock.connect((address, port))
-        for i in range(5):
-            sock.send("Hello".encode())
-            data = sock.recv(10)
-            print(data)
 
+        message = ""
+        type = input("Login or signup? 0 or 1")
+        message += type
+        #{username: “user1”, password: “1234"}
+        j = {}
+        j['username'] = "user1"
+        j['password'] = "1234"
+        if type == '1':
+            j['email'] = "email@gmail.com"
+
+        message += str(len(json.dumps(j))).zfill(4)
+        message += json.dumps(j)
+        msg = ''.join(format(ord(x), '08b') for x in message)
+        print(msg)
+        binary = "00110000 00110000 00110000 00110100 00110001 01111011 00100010 01110101 01110011 01100101 01110010 01101110 01100001 01101101 01100101 00100010 00111010 00100000 00100010 01110101 01110011 01100101 01110010 00110001 00100010 00101100 00100000 00100010 01110000 01100001 01110011 01110011 01110111 01101111 01110010 01100100 00100010 00111010 00100000 00100010 00110001 00110010 00110011 00110100 00100010 01111101"
+        print(binary == msg)
+        print(message)
+        sock.send(msg.encode())
+        print(sock.recv(1000, 0))
 
 
 if __name__ == "__main__":
