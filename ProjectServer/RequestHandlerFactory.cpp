aff92d641dcac2a07596b0e8f51b84445a96bea9
unknown <neslioz07@gmail.com>
2024-04-15 11:33:33 +0300

    Added RequestHandlerFactory implementation

--- ProjectServer/RequestHandlerFactory.cpp
+++ ProjectServer/RequestHandlerFactory.cpp
@@ -1,9 +1,22 @@
 #include "RequestHandlerFactory.h"
 
+RequestHandlerFactory::RequestHandlerFactory(IDatabase* database)
+    : m_database(database)/*, m_loginManager(database)*/
+{
+    if (m_database == nullptr) 
+    {
+        throw std::invalid_argument("Invalid Database pointer");
+    }
+}
+
+RequestHandlerFactory::~RequestHandlerFactory()
+{
+}
+
 // Creates login request manager
 LoginRequestHandler* RequestHandlerFactory::createLoginRequestHandler()
 {
-    return &LoginRequestHandler(*this);
+    return new LoginRequestHandler(*this);
 }
 
 // Return the login manager
