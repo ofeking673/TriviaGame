268a23d7c142eef6cd6805a767a9a231eca60b16
unknown <neslioz07@gmail.com>
2024-04-16 17:44:34 +0300

    Added MenuRequestHandler header file"

--- ProjectServer/MenuRequestHandler.h
+++ ProjectServer/MenuRequestHandler.h
@@ -1,14 +1,26 @@
 #pragma once
 #include "IRequestHandler.h"
+#include "LoggedUser.h"
+#include "RequestHandlerFactory.h"
 
 class MenuRequestHandler : public IRequestHandler
 {
 public:
-	MenuRequestHandler() = default;
-	virtual ~MenuRequestHandler() = default;
+	MenuRequestHandler();
+	virtual ~MenuRequestHandler();
 
-	virtual bool isRequestRelevant(Requestinfo requestInfo) { return true; }
-	virtual RequestResult HandleRequest(Requestinfo requestInfo) { return RequestResult(); }
+	virtual bool isRequestRelevant(Requestinfo requestInfo);
+	virtual RequestResult HandleRequest(Requestinfo requestInfo);
+
+	RequestResult signuot(Requestinfo requestInfo);
+	RequestResult getRooms(Requestinfo requestInfo);
+	RequestResult getPlayersInRoom(Requestinfo requestInfo);
+	RequestResult getPersonalStats(Requestinfo requestInfo);
+	RequestResult getHighScore(Requestinfo requestInfo);
+	RequestResult joinRoom(Requestinfo requestInfo);
+	RequestResult createRoom(Requestinfo requestInfo);
 
 private:
+	LoggedUser m_user;
+	RequestHandlerFactory& m_handlerFactory;
 };
