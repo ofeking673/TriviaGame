ea102f079770f6f5494bb2101fe8ffa9ba00ba0f
unknown <neslioz07@gmail.com>
2024-04-16 18:31:45 +0300

    Expand IDatabase header file

--- ProjectServer/RequestHandlerFactory.h
+++ ProjectServer/RequestHandlerFactory.h
@@ -1,19 +1,36 @@
 #pragma once
 #include "LoginManager.h"
 #include "IDatabase.h"
+#include "RoomManager.h"
+#include "StatisticsManager.h"
 
 class LoginRequestHandler;
+class MenuRequestHandler;
 
 class RequestHandlerFactory
 {
 public:
+	// TO-DO: Need to change - added room and statistics
 	RequestHandlerFactory(IDatabase* database);
 	~RequestHandlerFactory();
 
+	// Login & Signup related
 	LoginRequestHandler* createLoginRequestHandler();
 	LoginManager& getLoginManager();
 
+	// Room & statistics related
+	MenuRequestHandler* createMenuRequestHandler(LoggedUser);
+	StatisticsManager& getStatisticsManager();
+	RoomManager& getRoomManager();
+
 private:
-	LoginManager m_loginManager;
 	IDatabase* m_database;
+
+	// Login & Signup related
+	LoginManager m_loginManager;
+
+	// Room & statistics related
+	RoomManager m_roomManager;
+	StatisticsManager m_statisticsManager;
+
 };
