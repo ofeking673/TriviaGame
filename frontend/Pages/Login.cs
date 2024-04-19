90d77b5a12783e4c6f8e982ed9c42bae388528fb
Ofek Dory <ofeking475@gmail.com>
2024-04-19 15:07:02 +0300

    Added Login and signup pages + server communications

---
+++ frontend/Pages/Login.cs
@@ -0,0 +1,49 @@
+﻿using frontend.utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net.Sockets;
+using Newtonsoft.Json.Linq;
+
+namespace frontend.Pages
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoginUser user = new LoginUser(this.textBox1.Text, this.textBox2.Text);
+            string json = JsonConvert.SerializeObject(user);
+            string finalJson = $"0{json.Length.ToString().PadLeft(4, '0')}{json}";
+            Console.WriteLine(finalJson);
+            string binary = Utils.StringToBinary(finalJson);
+            Console.WriteLine(binary);
+            byte[] bytes = ASCIIEncoding.ASCII.GetBytes(binary);
+
+            Program.networkStream.Write(bytes, 0, bytes.Length);
+
+            byte[] bytes1 = new byte[1024];
+            Program.networkStream.Read(bytes1, 0, bytes1.Length);
+            string answer = Utils.GetBytesFromBinaryString(Encoding.Default.GetString(bytes1));
+            if (!string.IsNullOrEmpty(answer)) { this.Close(); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Signup signup = new Signup();
+            signup.ShowDialog();
+        }
+    }
+}
---
+++ frontend/Pages/Signup.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace frontend.Pages
+{
+    partial class Signup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(137, 125);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Password:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(137, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Username:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(309, 122);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(350, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(309, 28);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(350, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(137, 227);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Email:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(309, 224);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(350, 23);
+            textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(255, 325);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 11;
+            button1.Text = "Signup";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(477, 325);
+            button2.Name = "button2";
+            button2.Size = new Size(101, 23);
+            button2.TabIndex = 12;
+            button2.Text = "Back to login";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Signup
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Signup";
+            Text = "Signup";
+            Load += Signup_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+    }
+}
