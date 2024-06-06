57f1271c69d8e6bc2e98b281fee8692d49f8950b
ofeking475 <ofeking475@gmail.com>
2024-06-06 16:57:06 +0300

    Added my own front-end from my end of development, added matchmaking logic, now only need to configurate the codes used.

---
+++ frontend/Pages/Matchmaking.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace frontend.Pages
+{
+    partial class Matchmaking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Matchmaking));
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(384, 263);
+            label1.Name = "label1";
+            label1.Size = new Size(178, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Waiting for an opponent..";
+            // 
+            // Matchmaking
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(958, 540);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            Name = "Matchmaking";
+            Text = "Matchmaking";
+            Load += Matchmaking_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
